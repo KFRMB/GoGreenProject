ed83d4f00819922737532fc1f91bc009ba20f7de
KevinFrambach <kevin.frambach@gmail.com>
2018-01-09 15:18:12 +0100

    Servo Controller code
    Deze code heb ik gebruikt als tutorial voor mijn servo controller

---
+++ ServoController/ServoController.ino
@@ -0,0 +1,24 @@
+
+#include <Servo.h>
+
+Servo myservo;
+
+int pos = 0;
+
+void setup() {
+  myservo.attach(9);
+}
+
+void loop() {
+  for (pos = 0; pos <= 90; pos += 1) { 
+    myservo.write(pos);
+    delay(15);
+  }
+  delay(1500);
+  
+  for (pos = 90; pos >= 0; pos -= 1) {
+    myservo.write(pos);              
+    delay(15);
+  }
+  delay(1500);
+}
